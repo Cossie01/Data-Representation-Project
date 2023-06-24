40f2ac003621e5d74b5a11833612809616e3c108
Ciara <G00398379@gmit.ie>
2023-06-24 12:37:36 +0100

    Connecting to external API

--- Project.py/application.py
+++ Project.py/application.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify, abort, request
+from flask import Flask, jsonify, abort, request, render_template
 from voteDAO import voteDAO
+import requests
 
 app = Flask(__name__, static_url_path='', static_folder='static')
 
@@ -10,6 +11,21 @@ teas = [
     {'name':'Tetleys'},
     ]
 
+#External Api
+@app.route('/external-teas', methods=['GET'])
+def getExternalTeas():
+    try:
+        api_url = 'https://boonakitea.cyclic.app/api/teas'
+        response = requests.get(api_url)
+        response.raise_for_status()  
+        teas = response.json()
+        for tea in teas:
+            print(teas)
+        return render_template('externalApi.html', teas=teas)
+    except requests.exceptions.RequestException as e:
+        abort(500, f"An error occurred while retrieving teas from the external API: {e}")
+
+#Internal Api
 @app.route('/tea', methods = ['GET'])
 def getAllTeas():
     return jsonify(teas)
